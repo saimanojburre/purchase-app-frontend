d53e0f347f6eb5acd0e8fb0950e21d4066e8b130
Sai Manoj <saimanoj.burre@gmail.com>
2026-04-29 14:46:09 +0530

    added env.prod

--- src/app/services/purchase.ts
+++ src/app/services/purchase.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',

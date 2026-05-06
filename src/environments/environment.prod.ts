286e0f8cc174826791ccb58a28001f9c75a7cbf4
Sai Manoj <saimanoj.burre@gmail.com>
2026-05-06 12:59:34 +0530

    added gateway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   productiom: true,
-  apiUrl: 'https://purchase-api-ekhdhaasfsfjc2cv.southindia-01.azurewebsites.net',
+  apiUrl: 'https://purchase-gateway.azure-api.net',
 };

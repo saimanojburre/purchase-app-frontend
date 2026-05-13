5c4a68ac451adc504524fb79302f6ff51c53bd9b
Sai Manoj <saimanoj.burre@gmail.com>
2026-05-13 14:52:57 +0530

    env changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   productiom: true,
-  apiUrl: 'https://purchase-gateway.azure-api.net',
+  // apiUrl: 'https://purchase-gateway.azure-api.net',
+  apiUrl: 'https://purchase-api-ekhdhaasfsfjc2cv.southindia-01.azurewebsites.net',
 };
